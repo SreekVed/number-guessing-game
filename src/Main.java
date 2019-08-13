2a1b3951d8ab73d7fa0fea742c86558255e56d3a
Sreekar Vedula <54064887+SreekarV@users.noreply.github.com>
2019-08-13 18:01:42 +0530

    Initiates GuessingGame

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 public class Main {
     public static void main(String[] args) {
         GuessingGame game = new GuessingGame();
+        // The guessing range can be specified below. By default, it is 1-100.
         game.play(1,100);
     }    
 }
